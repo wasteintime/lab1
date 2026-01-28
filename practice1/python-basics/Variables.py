fb6eca2df8258d40fb2beb91e8d5edb2a456c0f8
wasteintime <zumalizanibek@gmail.com>
2026-01-28 22:18:03 +0500

    variables

---
+++ practice1/python-basics/Variables.py
@@ -0,0 +1,40 @@
+a = 1
+b = "Zhanibek"
+c = 2
+d = "Zhumali"
+e = 2006
+# Variables of big letter will not overwrite
+A = 11
+
+B = "Zhumali"
+
+C = "22"
+
+D = "Zhanibek"
+
+E = 2026
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print(A)
+print(B)
+print(C)
+print(D)
+print(E)
+
+f = str(52)    # f will be '52'
+g = int(1)    # g will be 1
+h = float(4)  # h will be 4.0
+l = str(12) # l will be '12'
+k = int(20) # k will be 20
+
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(l))
+print(type(k))
+
