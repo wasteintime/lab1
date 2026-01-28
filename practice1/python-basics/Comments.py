c3e31d9d13b0e7998f7f2ea4705b88ecc6fd8d59
wasteintime <zumalizanibek@gmail.com>
2026-01-28 22:09:15 +0500

    comments edit

---
+++ practice1/python-basics/Comments.py
@@ -0,0 +1,41 @@
+#This is a comment for print.
+print("Hello Kbtu") #This is a comment too
+
+#print("lets play")
+print("lets play") #This is a comment for lets play
+
+#This is a comment last example for this type
+
+"""
+This is a multi line
+comment 
+for pp2 example 1
+"""
+print("Multiline comments example 1")
+
+"""
+This is a multi line
+comment 
+for pp2 example 2 of comments
+"""
+
+print("Multiline comments example 2")
+
+"""
+the weather is
+good 
+for learning 
+python
+"""
+print("Multiline comments example 5")
+
+"""
+i love 
+python
+"""
+
+"""
+this is the 
+last 
+multi comment
+"""
