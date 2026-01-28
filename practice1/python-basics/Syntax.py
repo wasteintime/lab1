9c56f56556e831554c50b5e76ff46ee3dcf6f0dc
wasteintime <zumalizanibek@gmail.com>
2026-01-28 22:02:33 +0500

    syntax

---
+++ practice1/python-basics/Syntax.py
@@ -0,0 +1,18 @@
+#This is a comment for x.
+x = 9 
+#This is a comment for y.
+y = "KBTU SITE" 
+
+#This is a comment for z.
+z = 11
+#This is a comment for a.
+a = "Auto" 
+
+#This is a comment for b.
+b = "learning syntax" 
+
+#example of mistake.
+example1 = "if 3 > 1:"
+example2 = "print three is greater than two"
+example3 = "if 4 > 1:"
+example4 = "print four is greater than one"
