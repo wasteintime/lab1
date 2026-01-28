24b10af1c2348db1dd0e011608b3a63d863a83ea
wasteintime <zumalizanibek@gmail.com>
2026-01-28 22:25:34 +0500

    new commit

---
+++ practice1/python-basics/Data_Types.py
@@ -0,0 +1,16 @@
+a = 5
+b = "kbtu"
+c = 6.5
+d = False 
+e = ["Ali", "Beka" , "Zhan"] 
+A = int(30)
+B = str("7.4")
+C = bool(3)
+D = list(("Cat" , "Dog"))
+E = float(7.4) 
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
