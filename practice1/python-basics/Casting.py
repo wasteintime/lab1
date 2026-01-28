59bca8e658653836f48b528ca298702246e496cf
wasteintime <zumalizanibek@gmail.com>
2026-01-28 22:38:38 +0500

    final casting

---
+++ practice1/python-basics/Casting.py
@@ -0,0 +1,17 @@
+x1 = int(12)   # x1 will be 12
+x2 = int(4.5) # x2 will be 4
+x3 = int("23") # x3 will be 23
+x4 = int(5.8) # x4 will be 5
+x5 = int("22") # x5 will be 22
+
+x = float(4)     # x will be 4.0
+y = float(3.6)   # y will be 3.6
+z = float("4")   # z will be 4.0
+w = float("6.7") # w will be 6.7
+v = float("9.0") # w will be 9.0
+
+m = str("s1") # m will be 's1'
+y2 = str(34) # y2 will be '34'
+z2 = str("5.5") # z2 will be '5.5'
+w2 = str("cat") # w2 will be 'cat'
+v2 = str("34.65") # v2 will be '34.65'
